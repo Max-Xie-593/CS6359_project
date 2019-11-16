5f445b0016c3cda4bfb88e053d37621d1bf4fd70
mugdha-gupta <mxg167030@utdallas.edu>
2019-11-16 16:58:08 -0600

    refactored matching to use state desgin pattern

--- app/src/main/java/andr/mentorapp/NoExpertCheckedInGenTutorAvailState.kt
+++ app/src/main/java/andr/mentorapp/NoExpertCheckedInGenTutorAvailState.kt
@@ -4,7 +4,22 @@ import andr.mentorapp.Database.StudentUser
 import andr.mentorapp.Database.TutorUser
 import andr.mentorapp.GetHelpController.tutorSessions
 
+/**
+ * TutorState when there is not no expert for a given course checked in but a general tutor is available
+ *
+ * @author Mugdha Gupta
+ * @date 11/16/2019
+ */
 class NoExpertCheckedInGenTutorAvailState : TutorState {
+    /**
+     * Overrides getHelp function to add tutor student course triple to the tutor sessions and returns true
+     * if a new tutorSession was sucessfully made
+     *
+     * @param tutorUser         TutorUser? to match the student with
+     * @param studentUser       StudentUser to match with the tutor off the queue with
+     * @param course            Course that will be the subject of the turor session
+     * @return Boolean
+     */
     override fun getHelp(tutorUser: TutorUser?, studentUser: StudentUser, course: Course): Boolean {
         if (tutorUser != null){
             tutorSessions.add(Triple(tutorUser, studentUser, course))
--- app/src/main/java/andr/mentorapp/NoExpertCheckedInNoGenTutorAvailState.kt
+++ app/src/main/java/andr/mentorapp/NoExpertCheckedInNoGenTutorAvailState.kt
@@ -4,7 +4,22 @@ import andr.mentorapp.Database.StudentUser
 import andr.mentorapp.Database.TutorUser
 import andr.mentorapp.GetHelpController.studentQueue
 
+/**
+ * TutorState when there is not no expert for a given course checked in and no gerneral tutor either
+ *
+ * @author Mugdha Gupta
+ * @date 11/16/2019
+ */
 class NoExpertCheckedInNoGenTutorAvailState : TutorState {
+    /**
+     * Overrides getHelp function to add student course pair to the waiting queue and returns false
+     * since the student has not yet gotten help
+     *
+     * @param tutorUser         TutorUser? to match the student with
+     * @param studentUser       StudentUser to match with the tutor off the queue with
+     * @param course            Course that will be the subject of the turor session
+     * @return Boolean
+     */
     override fun getHelp(tutorUser: TutorUser?, studentUser: StudentUser, course: Course) : Boolean {
         studentQueue.add(Pair(studentUser, course))
         return false
